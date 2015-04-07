aa94ac11288ae8acc86ce5f03578e2c154c6eb26
wolffzheng <wolff_zheng@hotmail.com>
2015-04-08 09:42:23 +1000

    add books

--- upload.sh
+++ upload.sh
@@ -2,11 +2,15 @@
 git branch tmp
 git checkout tmp
 git add . --all
-echo "please input commit message:"
-read message
-git commit -m "$message"
+#echo "please input commit message:"
+#read message
+git commit -m "add books"
 git checkout master
 git merge tmp
 git push origin master
+expect "Username for 'https://github.com':"
+send "wolffzheng\r"
+expect "Password for 'https://wolffzheng@github.com':"
+send "Wolff8341\r"
 git branch -d tmp
 
